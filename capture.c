6905f2a1bbb7cde4504a5e581144855f04515bcd
Alex Norman <x37v.alex@gmail.com>
2012-09-03 12:37:36 -0700

    simple gamma correction

--- capture.c
+++ capture.c
@@ -39,6 +39,19 @@ struct buffer {
    size_t length;
 };
 
+uint8_t gamma_table[256] = {
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 8, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 10, 11, 11, 11, 11, 12, 12, 12, 13, 13, 13, 13, 14, 14, 14, 15, 15, 15, 16, 16, 16, 16, 17, 17, 18, 18, 18, 19, 19, 19, 20, 20, 20, 21, 21, 22, 22, 22, 23, 23, 24, 24, 24, 25, 25, 26, 26, 27, 27, 28, 28, 29, 29, 29, 30, 30, 31, 31, 32, 33, 33, 34, 34, 35, 35, 36, 36, 37, 37, 38, 39, 39, 40, 40, 41, 42, 42, 43, 43, 44, 45, 45, 46, 47, 47, 48, 49, 49, 50, 51, 51, 52, 53, 54, 54, 55, 56, 56, 57, 58, 59, 60, 60, 61, 62, 63, 64, 64, 65, 66, 67, 68, 69, 69, 70, 71, 72, 73, 74, 75, 76, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 99, 100, 101, 102, 103, 104, 105, 106, 107, 109, 110, 111, 112, 113, 115, 116, 117, 118, 119, 121, 122, 123, 124, 126, 127
+};
+
+inline uint8_t gamma_map(int v) {
+  if (v >= 256)
+    return 0x7F;
+  else if (v < 0)
+    return 0;
+  else
+    return gamma_table[v];
+}
+
 static char *           dev_name        = NULL;
 static char *           output_name        = NULL;
 static io_method io  = IO_METHOD_MMAP;
@@ -143,9 +156,12 @@ static void process_image(struct buffer * b) {
 #if 1
        ycrcb2rgb(y, cb, cr, &r, &g, &b);
        //ycrcb2rgb(y, cr, cb, &r, &g, &b);
-       rgb_buffer[0 + 3 *(col + row * width)] = 0x7F & (g >> 1);
-       rgb_buffer[1 + 3 *(col + row * width)] = 0x7F & (r >> 1);
-       rgb_buffer[2 + 3 *(col + row * width)] = 0x7F & (b >> 1);
+       rgb_buffer[0 + 3 *(col + row * width)] = gamma_map(g);
+       rgb_buffer[1 + 3 *(col + row * width)] = gamma_map(r);
+       rgb_buffer[2 + 3 *(col + row * width)] = gamma_map(b);
+       //rgb_buffer[0 + 3 *(col + row * width)] = 0x7F & (g >> 1);
+       //rgb_buffer[1 + 3 *(col + row * width)] = 0x7F & (r >> 1);
+       //rgb_buffer[2 + 3 *(col + row * width)] = 0x7F & (b >> 1);
 #else
        rgb_buffer[0 + 3 *(col + row * width)] = 
           rgb_buffer[1 + 3 *(col + row * width)] = 
