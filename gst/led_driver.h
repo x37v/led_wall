cfec089dd6feb427d756c83d3f217ffb2185ffe3
Alex Norman <x37v.alex@gmail.com>
2012-10-03 20:45:10 -0700

    trying a c version of the library

--- gst/led_driver.h
+++ gst/led_driver.h
@@ -3,15 +3,9 @@
 
 #include <inttypes.h>
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-   int led_open_output(char * device_path, unsigned int num_leds);
-   //one byte per r,g,b per pixel, so 3 * num_leds uint8_t. 
-   //starting upper left drawing rows at a time.
-   void led_write_buffer(uint8_t * rgb_buffer);
-#ifdef __cplusplus
-}
-#endif
+int led_open_output(char * device_path, unsigned int num_leds);
+//one byte per r,g,b per pixel, so 3 * num_leds uint8_t. 
+//starting upper left drawing rows at a time.
+void led_write_buffer(uint8_t * rgb_buffer);
 
 #endif
