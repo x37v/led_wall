76e6b1af2baa9b8d160fa7173b3eb70367718052
Alex Norman <x37v.alex@gmail.com>
2012-10-09 23:19:42 -0700

    doing some interface setup and cleanup

--- gst/led_driver.c
+++ gst/led_driver.c
@@ -9,6 +9,11 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+#include <errno.h>
+#include <termios.h>
+#include <unistd.h>
+
+
 
 //FILE * serial = NULL;
 int serial = -1;
@@ -18,6 +23,64 @@ uint8_t * led = NULL;
 #ifdef COMPUTE_FRAME_DELAY
 struct timeval time_last;
 #endif
+
+//set_interface_attribs and set_blocking from http://stackoverflow.com/questions/6947413/how-to-open-read-and-write-from-serial-port-in-c
+
+int set_interface_attribs (int fd, int speed, int parity) {
+  struct termios tty;
+  memset (&tty, 0, sizeof tty);
+  if (tcgetattr (fd, &tty) != 0)
+  {
+    fprintf(stderr, "error %d from tcgetattr", errno);
+    return -1;
+  }
+
+  cfsetospeed (&tty, speed);
+  cfsetispeed (&tty, speed);
+
+  tty.c_cflag = (tty.c_cflag & ~CSIZE) | CS8;     // 8-bit chars
+  // disable IGNBRK for mismatched speed tests; otherwise receive break
+  // as \000 chars
+  tty.c_iflag &= ~IGNBRK;         // ignore break signal
+  tty.c_lflag = 0;                // no signaling chars, no echo,
+  // no canonical processing
+  tty.c_oflag = 0;                // no remapping, no delays
+  tty.c_cc[VMIN]  = 0;            // read doesn't block
+  tty.c_cc[VTIME] = 5;            // 0.5 seconds read timeout
+
+  tty.c_iflag &= ~(IXON | IXOFF | IXANY); // shut off xon/xoff ctrl
+
+  tty.c_cflag |= (CLOCAL | CREAD);// ignore modem controls,
+  // enable reading
+  tty.c_cflag &= ~(PARENB | PARODD);      // shut off parity
+  tty.c_cflag |= parity;
+  tty.c_cflag &= ~CSTOPB;
+  //tty.c_cflag &= ~CRTSCTS;
+
+  if (tcsetattr (fd, TCSANOW, &tty) != 0)
+  {
+    fprintf(stderr, "error %d from tcsetattr", errno);
+    return -1;
+  }
+  return 0;
+}
+
+void set_blocking (int fd, int should_block) {
+  struct termios tty;
+  memset (&tty, 0, sizeof tty);
+  if (tcgetattr (fd, &tty) != 0) {
+    fprintf(stderr, "error %d from tggetattr", errno);
+    return;
+  }
+
+  tty.c_cc[VMIN]  = should_block ? 1 : 0;
+  tty.c_cc[VTIME] = 5;            // 0.5 seconds read timeout
+
+  if (tcsetattr (fd, TCSANOW, &tty) != 0)
+    fprintf(stderr, "error %d setting term attributes", errno);
+}
+
+
 
 uint8_t reverse_bits(uint8_t b) {
    return
@@ -103,12 +166,21 @@ int led_open_output(char * device_path, unsigned int led_count) {
       fprintf(stderr, "cannot open serial\n");
       return 0;
    }
+   set_interface_attribs(serial, B115200, 0);  // set speed to 115,200 bps, 8n1 (no parity)
+   set_blocking(serial, 1);                // set blocking
 
 #ifdef COMPUTE_FRAME_DELAY
    gettimeofday(&time_last, NULL);
 #endif
 
    return 1;
+}
+
+void led_close() {
+  if (serial != -1) {
+    close(serial);
+    serial = -1;
+  }
 }
 
 void led_write_buffer(uint8_t * rgb_buffer) {
